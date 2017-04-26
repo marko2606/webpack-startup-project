de0fb40294d2a6f12f79ec31d32ea8ba99dfc247
Marko <marko2606@hotmail.com>
2017-04-26 09:34:23 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            title: 'Webpack Workshop'
+        })
+    ]
+
+};
